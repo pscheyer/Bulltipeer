abbc576c1171c1cc5f840fe0135740380e22b0c5
Peter Scheyer <pscheyer@gmail.com>
2015-09-06 11:38:28 -0500

    Initial Commit

---
+++ Bulltipeer/Bulltipeer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bulltipeer
+//
+//  Created by Peter Scheyer on 9/6/15.
+//  Copyright (c) 2015 Peter Scheyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

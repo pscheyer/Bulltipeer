f8de52d66dbdf5bc6ee07987b8badf141722778d
Peter Scheyer <pscheyer@gmail.com>
2015-09-24 21:34:01 -0500

    20150924 2133CST adopt required protocols tableviewdelegate tableviewdatasource and actionsheetdelegate in FileSharingViewController.h, link tblFiles property to table view in main.storyboard.

--- Bulltipeer/Bulltipeer/FileSharingViewController.h
+++ Bulltipeer/Bulltipeer/FileSharingViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FileSharingViewController : UIViewController
+@interface FileSharingViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tblFiles;
 
